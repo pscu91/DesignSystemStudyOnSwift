c07e69d322e64be53b3950d50f0fc81ede387643
pscu91 <91787174+pscu91@users.noreply.github.com>
2023-08-21 15:41:45 +0900

    Initial commit

---
+++ DesignSystemStudyOnSwift/DesignSystemStudyOnSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignSystemStudyOnSwiftApp.swift
+//  DesignSystemStudyOnSwift
+//
+//  Created by SUNGIL-POS on 2023/08/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignSystemStudyOnSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
